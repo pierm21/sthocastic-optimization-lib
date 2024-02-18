1fc3d1e33fe6beea084cdbf6810da5bb2275261a
ague01 <aguerrini01@gmail.com>
2024-02-18 16:23:45 +0000

    factory first try added

---
+++ include/OptimizerFactory.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <memory>
+
+#include "Optimizer.hpp"
+#include "Problem.hpp"
+#include "SASPSO/SASPSO.hpp"
+
+/**
+ * @brief Factory class to create a specific optimizer for the given problem
+ *
+ * @tparam dim the dimension of the space in which the problem is defined
+ */
+template <std::size_t dim>
+class OptimizerFactory
+{
+public:
+	/**
+	 * @brief Enumerator to represent the available optimizers
+	 * - SelfAdaptiveSPSO: Self Adaptive Particle Swarm Optimization
+	 */
+	enum OptimizerName
+	{
+		SelfAdaptiveSPSO
+	};
+
+	/**
+	 * @brief Method to create a specific optimizer for the given problem given the optimizer name.
+	 * Throws runtime error if the name is not available.
+	 * The optimizer is created with its default parameters.
+	 *
+	 * @param name the name of the choosen optimizer
+	 * @param problem the problem to be optimized
+	 * @return std::unique_ptr<Optimizer<dim>> the pointer to the created optimizer
+	 */
+	std::unique_ptr<Optimizer<dim>> create(OptimizerName name, Problem<dim>& problem)
+	{
+		if(name == OptimizerName::SelfAdaptiveSPSO)
+			return std::make_unique<SASPSO<dim>>(problem);
+		else
+			throw std::runtime_error("Invalid optimizer name");
+	}
+};
